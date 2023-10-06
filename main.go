57faedf8c546e76013e5e56a85d554339a289710
tmbrody <brodytaylor311@gmail.com>
2023-10-06 14:31:46 -0500

    Add new paths and functions to check sever health and request hits

--- main.go
+++ main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
-func main() {
-	mux := http.NewServeMux()
+type apiConfig struct {
+	fileserverHits int
+}
 
-	corsMux := middlewareCors(mux)
+func (cfg *apiConfig) resetCounterHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintf(w, "Hits have been reset to 0.")
+}
 
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: corsMux,
-	}
+func (cfg *apiConfig) requestCounterHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	response := fmt.Sprintf("Hits: %d", cfg.fileserverHits)
+	w.Write([]byte(response))
+}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		panic(err)
-	}
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
 }
 
 func middlewareCors(next http.Handler) http.Handler {
@@ -31,4 +40,38 @@ func middlewareCors(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+	var apiCfg apiConfig
+
+	mux := http.NewServeMux()
+	fileServer := http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServer))
+	corsMux := middlewareCors(mux)
+
+	mux.HandleFunc("/healthz", readinessHandler)
+	mux.HandleFunc("/metrics", apiCfg.requestCounterHandler)
+	mux.HandleFunc("/reset", apiCfg.resetCounterHandler)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
